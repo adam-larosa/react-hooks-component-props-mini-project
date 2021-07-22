312d1f9834e95f3e10c47ef8cb6675f53aa07b2f
adam-larosa <adam.larosa@flatironschool.com>
2021-07-21 19:54:28 -0700

    Included context for comments

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,21 +1,25 @@
 import Emojis from './Emojis'
 function Article({ title, date = "January 1, 1970", preview, minutes:mins }) {
-    //make emoji array (empty)
-    //use conditional logic to set emoji type
-    //push appropiate number of emojis into array
-    //smack into small element
+/*
+    Possible bonus strategies?
 
-    //made an empty variable
-    //use conditional to set emoji type
-    //count number of times needed
-    //make the element
-    //set content of element
-    //RENDER STUFF!!!
-    
-    //start off with logic in JSX!
-    //ternery in JSX?
-    // <small>{date} {condition ? oneFunction() : otherFunction()}</small>
 
+    make emoji array (empty)
+    use conditional logic to set emoji type
+    push appropiate number of emojis into array
+    smack into small element
+
+    made an empty variable
+    use conditional to set emoji type
+    count number of times needed
+    make the element
+    set content of element
+    RENDER STUFF!!!
+
+    start off with logic in JSX!
+    ternery in JSX?
+    <small>{date} {condition ? oneFunction() : otherFunction()}</small>
+*/
     return (
         <article>
             <h3>{title}</h3>
