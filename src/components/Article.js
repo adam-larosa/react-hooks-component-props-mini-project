c5fd7d211d93454c381cdf2e3d2158226b93ee58
adam-larosa <adam.larosa@flatironschool.com>
2021-07-21 20:04:18 -0700

    Moved comments to relevant component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,25 +1,6 @@
 import Emojis from './Emojis'
 function Article({ title, date = "January 1, 1970", preview, minutes:mins }) {
-/*
-    Possible bonus strategies?
 
-
-    make emoji array (empty)
-    use conditional logic to set emoji type
-    push appropiate number of emojis into array
-    smack into small element
-
-    made an empty variable
-    use conditional to set emoji type
-    count number of times needed
-    make the element
-    set content of element
-    RENDER STUFF!!!
-
-    start off with logic in JSX!
-    ternery in JSX?
-    <small>{date} {condition ? oneFunction() : otherFunction()}</small>
-*/
     return (
         <article>
             <h3>{title}</h3>
--- src/components/Emojis.js
+++ src/components/Emojis.js
@@ -1,3 +1,23 @@
+/*
+    Possible bonus strategies?
+
+
+    make emoji array (empty)
+    use conditional logic to set emoji type
+    push appropiate number of emojis into array
+    smack into small element
+
+    made an empty variable
+    use conditional to set emoji type
+    count number of times needed
+    make the element
+    set content of element
+    RENDER STUFF!!!
+
+    start off with logic in JSX!
+    ternery in JSX?
+    <small>{date} {condition ? oneFunction() : otherFunction()}</small>
+*/
 export default function Emojis({ m }) {
     const belowThirty = m < 30
     const emoji = belowThirty ? "☕️" : "🍱"
